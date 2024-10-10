de965e5317da80419f057d06b639b00e5c82210d
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2024-10-09 21:17:25 -0500

    Se agrego el proyecto

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Justin Imbaquingo");
+        System.out.println("Vivo en San Martin");
+        System.out.println("Mi numero de celular es 0962122064");
+        System.out.println("Me gusta el color negro");
+    }
+}
